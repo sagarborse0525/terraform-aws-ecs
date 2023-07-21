bcdca07114a8d85828e5ff125707c164c466df55
sagarborse0525 <92211637+sagarborse0525@users.noreply.github.com>
2023-07-21 20:08:35 +0530

    Updated

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    
+    }
+  }
+}
 provider "aws" {
   assume_role {
     role_arn     = "arn:aws:iam::108040610828:role/ec2-role"
