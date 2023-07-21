a6ec59f77ddefc6e4d59aadb7246b90d9525bfc0
sagarborse0525 <92211637+sagarborse0525@users.noreply.github.com>
2023-07-21 19:49:03 +0530

    Updated

---
+++ task_def.tf
@@ -0,0 +1,29 @@
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "httpd:latest"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+  }
+}
